715adfbe5cd81abdb7faa8e87c001d9279382a52
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-12-24 12:27:59 +0700

    The end of "Unwind Segues in iOS Storyboards"

--- UnwindSeguesDemo/ViewController.swift
+++ UnwindSeguesDemo/ViewController.swift
@@ -5,10 +5,19 @@
 //  Created by Yohannes Wijaya on 12/24/15.
 //  Copyright © 2015 Yohannes Wijaya. All rights reserved.
 //
+// Source: http://spin.atomicobject.com/2014/10/25/ios-unwind-segues/
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBAction Properties
+    
+    @IBAction func unwindToMainViewController(sender: UIStoryboardSegue) {
+        if sender.identifier == "unwindFromThirdVC" {
+            print("Coming from third view controller")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
